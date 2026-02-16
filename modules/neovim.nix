d0c83db61a7d693e45c0bc92bbd1981e722bb955
Victor Jacobs <victor@vjcbs.be>
2026-02-16 11:56:38 +0100

    Neovim csv

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -6,62 +6,82 @@
     vimAlias = true;
     vimdiffAlias = true;
 
-    plugins = with pkgs.vimPlugins; [
-      nvim-autopairs
-      (nvim-treesitter.withPlugins (p: [
-        p.nix
-        p.lua
-        p.go
-      ]))
-      telescope-nvim
+    plugins = with pkgs.vimPlugins; let
+      pluginWithConfig = plugin: config: {
+        inherit plugin config;
+        type = "lua";
+      };
+    in [
+      (pluginWithConfig nvim-autopairs ''
+        require('nvim-autopairs').setup()
+      '')
+      (pluginWithConfig (nvim-treesitter.withPlugins (p: [
+          p.nix
+          p.lua
+          p.go
+          p.csv
+        ])) ''
+          require('nvim-treesitter.configs').setup {
+            highlight = { enable = true },
+            incremental_selection = {
+              enable = true,
+              keymaps = {
+                init_selection = "<M-Up>",
+                node_incremental = "<M-Up>",
+                scope_incremental = false,
+                node_decremental = "<M-Down>",
+              },
+            },
+          }
+        '')
+      (pluginWithConfig telescope-nvim ''
+        local builtin = require('telescope.builtin')
+
+        local function find_files_from_project_root()
+          vim.fn.system("git rev-parse --is-inside-work-tree")
+
+          if vim.v.shell_error == 0 then
+            builtin.git_files()
+          else
+            builtin.find_files()
+          end
+        end
+
+        vim.keymap.set('n', '<leader>ff', find_files_from_project_root, { desc = 'Find files (Smart)' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>cmd', builtin.commands, { desc = "Command Palette" })
+
+        require('telescope').setup {
+          defaults = {
+            file_ignore_patterns = { "node_modules", ".git" },
+            layout_strategy = 'horizontal',
+            layout_config = {
+              prompt_position = "bottom",
+              width = 0.9, -- Take up 90% of screen
+            },
+          },
+        }
+      '')
+      # nvim-cmp
+      # cmp-cmdline
       plenary-nvim
       telescope-fzf-native-nvim
+      (pluginWithConfig csvview-nvim ''
+        require('csvview').setup()
+
+        -- Automatically enable for csv and tsv files
+        vim.api.nvim_create_autocmd("FileType", {
+          pattern = { "csv", "tsv" },
+          callback = function()
+            vim.cmd("CsvViewEnable")
+          end,
+        })
+      '')
     ];
 
     extraLuaConfig = ''
-      require("nvim-autopairs").setup {}
-
-      require('nvim-treesitter.configs').setup {
-        highlight = { enable = true },
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            init_selection = "<M-Up>",
-            node_incremental = "<M-Up>",
-            scope_incremental = false,
-            node_decremental = "<M-Down>",
-          },
-        },
-      }
-
-      local builtin = require('telescope.builtin')
-
-      local function find_files_from_project_root()
-        vim.fn.system("git rev-parse --is-inside-work-tree")
-
-        if vim.v.shell_error == 0 then
-          builtin.git_files()
-        else
-          builtin.find_files()
-        end
-      end
-
-      vim.keymap.set('n', '<leader>ff', find_files_from_project_root, { desc = 'Find files (Smart)' })
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-      vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-      require('telescope').setup {
-        defaults = {
-          file_ignore_patterns = { "node_modules", ".git" },
-          layout_strategy = 'horizontal',
-          layout_config = {
-            prompt_position = "bottom",
-            width = 0.9, -- Take up 90% of screen
-          },
-        },
-      }
-
       vim.opt.cursorline = true
       vim.opt.tabstop = 4
       vim.opt.shiftwidth = 4
