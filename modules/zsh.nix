49a0be90b8c63b95f4215c50b607fa594ce965fe
Victor Jacobs <victor@vjcbs.be>
2026-02-20 10:50:47 +0100

    Silence direnv

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -88,5 +88,11 @@ in {
     enable = true;
     enableZshIntegration = true;
     nix-direnv.enable = true;
+
+    config = {
+      global = {
+        hide_env_diff = true;
+      };
+    };
   };
 }
