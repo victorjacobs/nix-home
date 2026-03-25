029fd9e82271d529f20d676a3765752ec9e7c237
Victor Jacobs <victor@vjcbs.be>
2026-03-25 19:47:18 +0100

    Ssh config

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -4,6 +4,15 @@ _: {
     enableDefaultConfig = false;
 
     matchBlocks = {
+      "u565771.your-storagebox.de" = {
+        port = 23;
+        user = "u565771";
+        # Force modern kex algorithms, storagebox somehow wants to use old ones
+        extraOptions = {
+          "KexAlgorithms" = "sntrup761x25519-sha512@openssh.com,curve25519-sha256,curve25519-sha256@libssh.org";
+        };
+      };
+
       "*.vjcbs.be" = {
         user = "vjacobs";
       };
